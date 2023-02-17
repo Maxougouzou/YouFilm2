6c8f66f520d3faa15da5d5916e314d0a1ba8900a
LeVraiDeVraiMax <93374903+LeVraiDeVraiMax@users.noreply.github.com>
2023-02-17 18:11:25 +0100

    Update MovieController.php

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Movie;
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class MovieController extends AbstractController
 {
+
 
     }
