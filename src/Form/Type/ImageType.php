4f0048996065162179edf94a72d4808b647e53fa
kakylemakaky <115618811+kakylemakaky@users.noreply.github.com>
2023-02-19 17:38:47 +0100

    le upload image marche presque on a jamais été aussi proche

--- src/Form/Type/ImageType.php
+++ src/Form/Type/ImageType.php
@@ -19,7 +19,7 @@ class ImageType extends AbstractType
             ->add('Image')
             ->add('CreationDate')
             ->add('file', FileType::class, [
-                'label' => 'Image (JPG, PNG or GIF file)',
+                'label' => 'Image (JPG ou PNG)',
                 'required' => true,
                 'constraints' => [
                     new File([
