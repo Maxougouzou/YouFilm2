96fc922c793aabca02f1dac7effb942f17ec1dfc
LeVraiDeVraiMax <115991340+Maxougouzou@users.noreply.github.com>
2023-02-26 00:02:57 +0100

    Modif et suppression de son propre com

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -40,4 +40,55 @@ class CommentController extends AbstractController
 
         ]);
 
-    }}
+    }
+    #[Route('/comments/delete/{id}', name: 'app_delete_comment')]
+    public function deleteComment(Request $request, EntityManagerInterface $entityManager, Comment $comment): Response
+    {
+        $movie = $comment->getMovieId();
+        if (!$movie) {
+            // Le commentaire n'est associé à aucun film, renvoie une erreur ou redirige vers une autre page.
+            throw new \Exception('Comment is not associated with any movie.');
+        }
+
+        $movieId = $movie->getId(); // Récupère l'identifiant du film associé au commentaire
+
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Comment deleted successfully');
+
+        return $this->redirectToRoute('movie_show', ['id' => $movieId]);
+    }
+
+    #[Route('/comments/edit/{id}', name: 'app_edit_comment')]
+    public function editComment(Request $request, EntityManagerInterface $entityManager, Comment $comment): Response
+    {
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Comment edited successfully');
+
+            return $this->redirectToRoute('movie_show', ['id' => $comment->getMovieId()->getId()]);
+        }
+
+        return $this->render('comment/edit.html.twig', [
+            'form' => $form->createView(),
+            'comment' => $comment,
+            'movie'=>$comment->getMovieId(),
+        ]);
+    }
+
+
+
+
+
+
+
+
+
+}
+
