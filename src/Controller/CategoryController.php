15410dc825e33053dec0e1e4ea2865c6a57c0153
LeVraiDeVraiMax <115991340+Maxougouzou@users.noreply.github.com>
2023-03-03 17:27:30 +0100

    category dropmenu

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -18,10 +18,12 @@ class CategoryController extends AbstractController
     {
 
         $movies = $entityManager->getRepository(Movie::class)->findAll();
+        $user= $this->getUser();
 
         return $this->render('category/category.html.twig', [
             'category' => $category,
-            'movies' =>$movies
+            'movies' =>$movies,
+            'user'=>$user
         ]);
 
     }
